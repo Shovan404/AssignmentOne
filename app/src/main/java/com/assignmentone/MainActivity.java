314ffdaf8ec005c3ca603a96f396e9750ae81cd3
Shovan404 <shovanrj4@gmail.com>
2019-11-26 09:29:35 -0500

    Output

--- app/src/main/java/com/assignmentone/MainActivity.java
+++ app/src/main/java/com/assignmentone/MainActivity.java
@@ -2,6 +2,7 @@ package com.assignmentone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -15,21 +16,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    EditText etAdult, etChild, etRoom;
-    TextView tvCheckin, tvCheckout,tvVat, tvTotal, tvGross;
+    TextView tvCheckin, tvCheckout, tvTotal, tvVat, tvGross;
     Spinner spinRoom;
-    DatePickerDialog datePickerDialog;
-    Button btnCal,btnDiff;
-    Calendar c1,c2;
-    String strDiff;
+    EditText etAdult, etChild, etRoom;
+    Button btnCal;
+    AlertDialog.Builder builder;
 
-    String[] Room={"Delux(2000)","Presenditial(5000)","Primium(7000)"};
+    private String[] Room = {"Deluxe", "Presidential", "Premium"};
+    double total, grosstotal, vat, roomcost, noofday;
+    Date dateChecking, dateCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //binding
+        builder = new AlertDialog.Builder(this);
         tvCheckin = findViewById(R.id.tvCheckin);
         tvCheckout = findViewById(R.id.tvCheckout);
         tvVat = findViewById(R.id.tvVat);
@@ -45,117 +48,165 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etAdult = findViewById(R.id.etAdult);
         etChild = findViewById(R.id.etChild);
         etRoom = findViewById(R.id.etRoom);
-        spinRoom=findViewById(R.id.spinRoom);
+        spinRoom = findViewById(R.id.spinRoom);
+        btnCal = findViewById(R.id.btnCal);
 
-        btnCal=findViewById(R.id.btnCal);
-        btnCal.setOnClickListener(this);
 
-        btnDiff=findViewById(R.id.btnDiff);
-        btnDiff.setOnClickListener(this);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, Room);
+        spinRoom.setAdapter(adapter);
 
         tvCheckin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c1 = Calendar.getInstance();
-                int mYear = c1.get(Calendar.YEAR);
-                int mMonth = c1.get(Calendar.MONTH);
-                int mDay = c1.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(MainActivity.this,new DatePickerDialog.OnDateSetListener() {
-
-                    @Override
-                    public void onDateSet(DatePicker view, int year,
-                                          int monthOfYear, int dayOfMonth) {
-                        String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-                        String mon=MONTHS[monthOfYear];
-
-                        tvCheckin.setText(dayOfMonth + "-"
-                                + (mon) + "-" + year);
-
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                loadCheckingDate();
             }
         });
 
         tvCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c2 = Calendar.getInstance();
-                int mYear = c2.get(Calendar.YEAR);
-                int mMonth = c2.get(Calendar.MONTH);
-                int mDay = c2.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-                        String mon=MONTHS[monthOfYear];
-                        tvCheckout.setText(dayOfMonth + "-" + (mon) + "-" + year);
-
-
-
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                loadCheckoutDate();
             }
         });
 
-        ArrayAdapter adapterRoom = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, Room);
-        spinRoom.setAdapter(adapterRoom);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etAdult.getText())) {
+                    etAdult.setError("Enter number of adults");
+                    return;
+                }
+                if (TextUtils.isEmpty(etChild.getText())) {
+                    etChild.setError("Enter number of children");
+                    return;
+                }
+                if (TextUtils.isEmpty(etRoom.getText())) {
+                    etRoom.setError("Enter number of Rooms");
+                    return;
+                }
+                if (etAdult.getText().toString().equals("0")) {
+                    etAdult.setError("Adult cannot be 0");
+                    return;
+                }
+                if (etRoom.getText().toString().equals("0")) {
+                    etRoom.setError("Room cannot be 0");
+                    return;
+                }
+                if (dateChecking.before(dateCheckout)) {
+                    noofday = Double.parseDouble(CheckDate());
+                    CalculateGross();
+                }
+            }
+        });
+
+
     }
 
+    private void loadCheckingDate() {
+        final Calendar date1 = Calendar.getInstance();
+        int year = date1.get(Calendar.YEAR);
+        int month = date1.get(Calendar.MONTH);
+        int day = date1.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerChecking = new DatePickerDialog(
+                this, this, year, month, day);
+        datePickerChecking.show();
+    }
 
+    private void loadCheckoutDate() {
+        final Calendar date2 = Calendar.getInstance();
+        int year = date2.get(Calendar.YEAR);
+        int month = date2.get(Calendar.MONTH);
+        int day = date2.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerCheckOut = new DatePickerDialog(
+                this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "-" + month + "-" + year;
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                try {
+                    dateCheckout = format.parse(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                tvCheckout.setText(date);
+            }
+        }, year, month, day);
+        datePickerCheckOut.show();
+    }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btnCal:
-                double Vat, Total, GTotal;
-
-
-                Total=Double.parseDouble(tvTotal.getText().toString());
-                if (TextUtils.isEmpty(etAdult.getText())){
-                    etAdult.setError("Please enter number of adults");
-                    return;
-                }
-                if (TextUtils.isEmpty(etRoom.getText())){
-                    etRoom.setError("Please enter number of room");
-                    return;
-                }
-                Vat= (Total / 100.0f) * 13;
-
-                GTotal= Total + Vat;
-
-                tvTotal.setText("Total: "+Total+"");
-                tvVat.setText("Vat(13%): "+Vat+"");
-                tvGross.setText("Gross Total: "+GTotal+"");
-
-                break;
-
-            case R.id.btnDiff:
-                try {
-                    String d1 = tvCheckin.getText ().toString ();
-                    String d2 = tvCheckout.getText ().toString ();
-
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy");
-                    Date date1 = simpleDateFormat.parse(d1);
-                    Date date2 = simpleDateFormat.parse(d2);
-                    long difference = Math.abs(date1.getTime() - date2.getTime());
-
-                    long diffInDays = difference / (24 * 60 * 60 * 1000);
-
-                    strDiff=String. valueOf(diffInDays);
-                    Toast.makeText(MainActivity.this,strDiff,Toast.LENGTH_SHORT).show();
-
-                }
-                catch(Exception ex)
-                {
-
-                    ex.printStackTrace();
-                }
-                break;
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month = month + 1;
+        String date = dayOfMonth + "-" + month + "-" + year;
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            dateChecking = format.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+
+        tvCheckin.setText(date);
     }
 
+    private void CalculateGross() {
+        if (spinRoom.getSelectedItem().toString().equals("Deluxe")) {
+            roomcost = 2000;
+        } else if (spinRoom.getSelectedItem().toString().equals("Presidential")) {
+            roomcost = 5000;
+        } else if (spinRoom.getSelectedItem().toString().equals("Premium")) {
+            roomcost = 4000;
+        }
+        int noofroom = Integer.parseInt(etRoom.getText().toString());
+        total = roomcost * noofroom * noofday;
+        vat = (0.13 * total);
+        grosstotal = total + vat;
+        String setTotal = "Total : " + total;
+        String setVat = "Vat (13%) : " + vat;
+        String setGrossTotal = "Gross Total : " + grosstotal;
+        tvTotal.setText(setTotal);
+        tvVat.setText(setVat);
+        tvGross.setText(setGrossTotal);
+    }
+
+    private String CheckDate() {
+        Calendar cCal = Calendar.getInstance();
+        cCal.setTime(dateChecking);
+        int cYear = cCal.get(Calendar.YEAR);
+        int cMonth = cCal.get(Calendar.MONTH);
+        int cDay = cCal.get(Calendar.DAY_OF_MONTH);
+
+        Calendar eCal = Calendar.getInstance();
+        eCal.setTime(dateCheckout);
+        int eYear = eCal.get(Calendar.YEAR);
+        int eMonth = eCal.get(Calendar.MONTH);
+        int eDay = eCal.get(Calendar.DAY_OF_MONTH);
+
+        Calendar date1 = Calendar.getInstance();
+        Calendar date2 = Calendar.getInstance();
+
+        date1.clear();
+        date1.set(cYear, cMonth, cDay);
+        date2.clear();
+        date2.set(eYear, eMonth, eDay);
+
+        long diff = date2.getTimeInMillis() - date1.getTimeInMillis();
+        float dayCount = (float) diff / (24 * 60 * 60 * 1000);
+        return dayCount + "";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
